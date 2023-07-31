43515b67fd2ba4db9599b41e99b2de05c17e94f1
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-07-31 21:18:39 +0600

    animated cursor added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -4,12 +4,21 @@ import Footer from '../Shared/footer/Footer';
 import Navbar from '../Shared/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import NavTop from '../Shared/navbar/NavTop';
+import AnimatedCursor from "react-animated-cursor"
 
 const MainLayout = () => {
     return (
         <div>
             <NavTop/>
             <Navbar/>
+            <  AnimatedCursor   innerSize={17}
+      outerSize={8}
+      color='118, 167, 19'
+      outerAlpha={0.4}
+      innerScale={0.10}
+      outerScale={7}
+      
+      />
           <Outlet/>
             <Footer/>
         </div>
